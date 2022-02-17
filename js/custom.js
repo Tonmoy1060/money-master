7093b26488f0b588e14ed4dacdf22d00efd32617
Tomnoy1060 <abunaimtonmoy60@gmail.com>
2022-02-17 14:36:01 +0600

    html file done

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+document.getElementById('calculation-button').addEventListener('click', function(){
+    const foodExpences = document.getElementById('food-input');
+    const foodExpencesText = foodExpences.value;
+    const foodExpencesTextNumber = parseFloat(foodExpencesText);
+})
