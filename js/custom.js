907383d7c4d696b64f23a6566b41d2a9a5ae4de5
Tomnoy1060 <abunaimtonmoy60@gmail.com>
2022-02-18 14:54:50 +0600

    final

--- js/custom.js
+++ js/custom.js
@@ -15,7 +15,7 @@ function putInnerTextValue(textId, amount){
     const textField = document.getElementById(textId);
     const textFieldText = textField.innerText;
     const textFieldTextNumber = parseFloat(textFieldText);
-    const newTotalTextField =  amount + textFieldTextNumber
+    const newTotalTextField =  amount + textFieldTextNumber ;
 
     textField.innerText = newTotalTextField;
     return newTotalTextField
@@ -40,16 +40,16 @@ document.getElementById('calculation-button').addEventListener('click', function
     const clothesExpences = getInputValue('clothes-input', false);
 
     if(isNaN(income)){
-        alert('Please Give Me Money. Not number')
+        alert('Please Give Me Money. Not number');
     }
     else if(isNaN(foodExpences)){
-        alert('You took Food, Where is Money?')
+        alert('You took Food, Where is Money?');
     }
     else if(isNaN(rentExpences)){
-        alert('You took Rent, Where is Money?')
+        alert('You took Rent, Where is Money?');
     }
     else if(isNaN(clothesExpences)){
-        alert('You took Clothes, Where is Money?')
+        alert('You took Clothes, Where is Money?');
     }
     else if(income >= 1 && foodExpences >= 0 && rentExpences >= 0 &&clothesExpences >= 0 ){
         const inputExpences = foodExpences + rentExpences + clothesExpences;
@@ -60,12 +60,12 @@ document.getElementById('calculation-button').addEventListener('click', function
             const putBalance = getInnerBalanceTextValue('balance-field', income, putTotalExpenses);
         }
         else{
-            alert('You cant use money more than your Income')
+            alert('You cant use money more than your Income');
         }
 
     }
     else{
-        alert('No, NO, You Cant take money fromhere,give positive value.')
+        alert('No, NO, You Cant take money from here, give positive amount.');
     }
    
 })
@@ -80,22 +80,28 @@ document.getElementById('saving-button').addEventListener('click', function(){
     const savingInputToSavingAmount = (savingInput / 100) * income;
 
     if(isNaN(savingInput)){
-        alert('You can only save your Money, not your Name.')
+        alert('You can only save your Money, not your Name.');
     }
-    else if(savingInput > 0){
+    else if(savingInput >= 0){
         if(savingInputToSavingAmount <= putBalance){
             // put saving amount to saving amount field
-        const savingAmount = putInnerTextValue('saving-field', savingInputToSavingAmount);
-        // get remaining balance and put balance 
-        const remainingBalance = getInnerBalanceTextValue('remaining-field',putBalance, savingAmount);
+            const savingAmount = putInnerTextValue('saving-field', savingInputToSavingAmount);
+            if(savingAmount > 0){
+                // get remaining balance and put balance 
+                const remainingBalance = getInnerBalanceTextValue('remaining-field',putBalance, savingAmount);
+            }
+            else{
+                alert('You have no enough money to save');
+            }
+            
         }
         else{
-            alert('You cant Save More than Your Balance.')
+            alert('You cant Save More than Your Balance.');
         }
         
     }
     else{
-        alert('Be positive and save positive money.')
+        alert('Be positive and save positive money.');
     }
     
 })
