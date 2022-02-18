bd9e16a6d92b7d777e5f5742c054e00e72949b17
Tomnoy1060 <abunaimtonmoy60@gmail.com>
2022-02-18 11:44:53 +0600

    main work done

--- js/custom.js
+++ js/custom.js
@@ -1,42 +1,79 @@
-// function getInputValue(inputValue){
-//     const expenses = document.getElementById(inputValue);
-//     const expensesText = expenses.value;
-//     const expensesTextNumber = parseFloat(expensesText);
-//     expenses.value = '';
-//     return expensesTextNumber
-// }    
+// get all input field value
+function getInputValue(inputId, fixed){
+    const inputField = document.getElementById(inputId);
+    const inputFieldText = inputField.value;
+    const inputFieldTextNumber = parseFloat(inputFieldText);
+    if (fixed != false){
 
+    }
+    else{
+        inputField.value = '';
+    }
+    return inputFieldTextNumber
+}
+function putInnerTextValue(textId, amount){
+    const textField = document.getElementById(textId);
+    const textFieldText = textField.innerText;
+    const textFieldTextNumber = parseFloat(textFieldText);
+    const newTotalTextField =  amount + textFieldTextNumber
 
+    textField.innerText = newTotalTextField;
+    return newTotalTextField
+}
+function getInnerBalanceTextValue(textId, extraIncome, amount){
+    const textField = document.getElementById(textId);
+    const textFieldText = textField.innerText;
+    const newBalance = extraIncome - amount;
+    textField.innerText = newBalance; 
+}
+function updateBalance(balanceId){
+    const balance = document.getElementById(balanceId);
+    const balanceText = balance.innerText;
+    const balanceTextNumber = parseFloat(balanceText);
+    return balanceTextNumber
+}
 document.getElementById('calculation-button').addEventListener('click', function(){
-    // food input part
-    // const foodExpences = getInputValue('food-input');
-    // console.log(foodExpences);
-    const foodExpenses = document.getElementById('food-input');
-    const foodExpensesText = foodExpenses.value;
-    const foodExpensesTextNumber = parseFloat(foodExpensesText);
+    // total expences input sum
+    const income = getInputValue('income-input', true);
+    const foodExpences = getInputValue('food-input', false);
+    const rentExpences = getInputValue('rent-input', false);
+    const clothesExpences = getInputValue('clothes-input', false);
 
-    
-    // rent input part
-    // const rentExpences = getInputValue('rent-input');
-    // console.log(rentExpences);
-    const rentExpenses = document.getElementById('rent-input');
-    const rentExpensesText = rentExpenses.value;
-    const rentExpensesTextNumber = parseFloat(rentExpensesText);
-
-    // clothes input part
-    // const clothesExpences = getInputValue('clothes-input');
-    // console.log(clothesExpences);
-    const clothesExpenses = document.getElementById('clothes-input');
-    const clothesExpensesText = clothesExpenses.value;
-    const clothesExpensesTextNumber = parseFloat(clothesExpensesText);
-
-    // total expences sum input
-    const totalExpences = foodExpensesTextNumber + rentExpensesTextNumber + clothesExpensesTextNumber;
-
-    // total expenses get input
-    const expenses = document.getElementById('expences-input');
-    const expensesText = expenses.innerText;
-    const expensesTextNumber = parseFloat(expensesText);
-
-    expenses.innerText = totalExpences;
+    if(income != Number){
+        alert('Pelease give your valid Income In number')
+    }
+    else if(foodExpences != Number){
+        alert('Pelease give your valid Food Expenses In number')
+    }
+    else if( rentExpences != Number){
+        alert('Pelease give your valid Rent Expenses In number')
+    }
+    else if(clothesExpences != Number){
+        alert('Pelease give your valid Clothes Expenses In number')
+    }
+    else if(income >= 1 && foodExpences >= 0 && rentExpences >= 0 &&clothesExpences >= 0 ){
+        const inputExpences = foodExpences + rentExpences + clothesExpences;
+        // total input expenses get and put total expenses
+        const putTotalExpenses = putInnerTextValue('expences-field', inputExpences);
+        // balance put after expenses
+        const putBalance = getInnerBalanceTextValue('balance-field', income, putTotalExpenses);
+    }
+    else{
+        alert('Please give Valid Input')
+    }
+   
+})
+// saving part
+document.getElementById('saving-button').addEventListener('click', function(){
+    // income and get balance after expenses
+    const income = getInputValue('income-input');
+    const putBalance = updateBalance('balance-field');
+    // saving input
+    const savingInput = getInputValue('saving-input', false);
+    // saving amount from parsent
+    const savingInputToSavingAmount = (savingInput / 100) * income;
+    // put saving amount to saving amount field
+    const savingAmount = putInnerTextValue('saving-field', savingInputToSavingAmount);
+    // get remaining balance and put balance 
+    const remainingBalance = getInnerBalanceTextValue('remaining-field',putBalance, savingAmount);
 })
